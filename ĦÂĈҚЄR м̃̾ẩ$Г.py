a273482a8a8d85a50c5a65febf0bdbc1758aca20
Hacker M3sr <64711640+HackerM3sr@users.noreply.github.com>
2020-06-24 11:16:08 +0200

    Add files via upload

---
+++ ĦÂĈҚЄR м̃̾ẩ$Г.py
@@ -0,0 +1,439 @@
+
+import requests,time
+                           
+
+logo='''_                _
+| |__   __ _  ___| | _____ _ __   _ __ ___   __ _ ___ _ __
+| '_ \ / _` |/ __| |/ / _ \ '__| | '_ ` _ \ / _` / __| '__|
+| | | | (_| | (__|   <  __/ |    | | | | | | (_| \__ \ |
+|_| |_|\__,_|\___|_|\_\___|_|    |_| |_| |_|\__,_|___/_|'''
+print(logo)
+
+
+
+headers ={'Content-type': 'application/json; charset=UTF-8',
+                  'Accept': 'application/json',
+                  'User-Agent': 'okhttp/3.11.0 (Linux; U; Android 9; Redmi Note 6 Pro MIUI/V10.3.6.0.PEKMIXM)',
+                  'Host': 'nep.advangelists.com',
+'Connection': 'Keep-Alive',
+                  'Accept-Encoding': 'gzip'}
+
+
+url = 'http://nep.advangelists.com/xp/get?pubid=c744316a273490c2a25c212e6d0b4bf8'
+       
+
+
+
+headers ={'Content-type': 'application/json; charset=UTF-8',
+                  'Accept': 'application/json',
+                  'User-Agent': 'Mozilla/5.0 (Linux; Android 8.1.0; JKM-LX1 Build/HUAWEIJKM-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.90 Mobile Safari/537.36',
+                  'Host': 'voucher.waslabrowser.com',
+'referer': 'https://voucher.waslabrowser.com/',
+'Connection': 'Keep-Alive',
+                  'Accept-Encoding': 'gzip,deflate,br'}
+
+
+
+
+url = 'https://voucher.waslabrowser.com/static/css/main.32805de3.chunk.css'
+
+url = 'https://voucher.waslabrowser.com/favicon.ico'
+
+
+import webbrowser 
+webbrowser.open('https://www.youtube.com/channel/UCmXVGeICHXrarOB5KO5TxTg')
+
+print('Programming by Abdul Hafiz Muhammad')
+
+import webbrowser 
+webbrowser.open('https://www.youtube.com/channel/UCmXVGeICHXrarOB5KO5TxTg')
+
+print('Programming by Abdul Hafiz Muhammad')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.youtube.com/channel/UCmXVGeICHXrarOB5KO5TxTg')
+
+print('_______Done _______you win 7 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/KGM1iFN_nN8')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.facebook.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://www.google.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/AtDqybTngGs')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://www.facebook.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.google.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/vV8jP8S-yOM')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+
+import webbrowser 
+webbrowser.open('https://www.filfan.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/NmC3-baPz4k')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.your-freedom.net')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.yahoo.com')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://mail.yahoo.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.facebook.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://youtu.be/0-cVN-sNN00')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/JQvBFd64Nkw')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://www.gmail.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/3PV2FY6RkMQ')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/PZ_dN4sGvv8')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+
+
+import webbrowser 
+webbrowser.open('https://www.facebook.com')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+import webbrowser 
+webbrowser.open('https://www.youtube.com/channel/UCE26zVmbLig_XPZCWrnPT_A')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.file4.net')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.google.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/KGM1iFN_nN8')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.facebook.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://www.google.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/AtDqybTngGs')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://www.facebook.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.google.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/vV8jP8S-yOM')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+
+import webbrowser 
+webbrowser.open('https://www.filfan.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/NmC3-baPz4k')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.your-freedom.net')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.yahoo.com')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://mail.yahoo.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+import webbrowser 
+webbrowser.open('https://www.facebook.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://youtu.be/0-cVN-sNN00')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/JQvBFd64Nkw')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser
+webbrowser.open('https://www.gmail.com')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/3PV2FY6RkMQ')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://youtu.be/PZ_dN4sGvv8')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+
+
+import webbrowser 
+webbrowser.open('https://www.facebook.com')
+
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+import webbrowser 
+webbrowser.open('https://www.youtube.com/channel/UCE26zVmbLig_XPZCWrnPT_A')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+
+
+import webbrowser 
+webbrowser.open('https://www.file4.net')
+
+print('_______Done _______you win 4 points______')
+import time
+time.sleep(10)
+import os
+os.system("wasla script_v1.1.py")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
